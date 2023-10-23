8c4c0d44b70bde6f369677072a411ff82e1df888
Andriy Dubovich <86286088+AndriyDubovichWork@users.noreply.github.com>
2023-10-23 20:42:39 +0300

    login logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,24 @@
 
 import { postDream } from './api/dream/requests';
 import style from './home.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <main className={style.home}>
+      {session ? (
+        <>
+          <img
+            src={session?.user?.image || 'img'}
+            alt={session?.user?.name || 'name'}
+          />
+          <h1 onClick={() => signOut()}>log out</h1>
+        </>
+      ) : (
+        <h1 onClick={() => signIn()}>login</h1>
+      )}
       <input
         type='file'
         onChange={async (e) => console.log(await postDream(e.target.value))}
